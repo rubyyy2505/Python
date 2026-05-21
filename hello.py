b90e9a6f9633decc37f0b40b77daad8c0ab75e36
Your Name <you@example.com>
2026-05-21 07:45:31 +0545

    first program

---
+++ hello.py
@@ -0,0 +1,10 @@
+# print("hello world")
+# a=5 
+# b=4
+# print(a+b)
+# x=10
+# print(type(x))
+
+num1 = int(input("Enter an integer:"))
+num2 = float(input("Enter a float number:"))
+print(num1+num2)
