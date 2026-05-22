639062f7af6da5c8b2d071a88741ebe620c6f778
Your Name <you@example.com>
2026-05-22 07:22:10 +0545

    3rd pgm

---
+++ list.py
@@ -0,0 +1,12 @@
+mylist = ["rubina", "smriti", "ranjita", "anjila", "anjee"]
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+print(mylist[3])
+print(mylist[4])
+
+print(mylist[-1])
+print(mylist[-2])
+print(mylist[-3])
+print(mylist[-4])
+print(mylist[-5])
