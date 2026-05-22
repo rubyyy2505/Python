6a34b16e0a7321fc15abe2ec6634a89f1f80e5a5
Your Name <you@example.com>
2026-05-22 08:00:34 +0545

    3rd pgm

--- list.py
+++ list.py
@@ -1,12 +1,24 @@
-mylist = ["rubina", "smriti", "ranjita", "anjila", "anjee"]
-print(mylist[0])
-print(mylist[1])
-print(mylist[2])
-print(mylist[3])
-print(mylist[4])
+# mylist = ["rubina", "smriti", "ranjita", "anjila", "anjee"]
+# print(mylist[0])
+# print(mylist[1])
+# print(mylist[2])
+# print(mylist[3])
+# print(mylist[4])
 
-print(mylist[-1])
-print(mylist[-2])
-print(mylist[-3])
-print(mylist[-4])
-print(mylist[-5])
+# print(mylist[-1])
+# print(mylist[-2])
+# print(mylist[-3])
+# print(mylist[-4])
+# print(mylist[-5])
+
+#mylist = ["1", "2", "3", "4", "5", "6", "7"]
+#print(mylist[0:7])
+#print(mylist[6:2:-1])
+#print(mylist[::-3])
+
+# mytuple = ("apple", "banana", "grapes")
+# print(mytuple)
+# print(len(mytuple))
+
+mytuple = ("rubina" ,)
+print(type(mytuple))
